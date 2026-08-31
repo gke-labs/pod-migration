a97d2052bbf2db84f5e10ac56dcd698f8fb78004
Yao Luo <yaoluo@google.com>
2026-08-31 14:43:11 +0000

    feat(ha): add topology spread constraints and strip managedFields in cache

--- controller/cmd/main.go
+++ controller/cmd/main.go
@@ -35,6 +35,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -80,6 +81,9 @@ func main() {
 		LeaderElectionID:              "pod-migration-leader.gke.io",
 		LeaderElectionNamespace:       "pod-migration-system",
 		LeaderElectionReleaseOnCancel: true,
+		Cache: cache.Options{
+			DefaultTransform: cache.TransformStripManagedFields(),
+		},
 		// Webhook server is on :9443 by default.
 		WebhookServer: webhook.NewServer(webhook.Options{Port: 9443}),
 	})
